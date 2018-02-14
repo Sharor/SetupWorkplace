e7f828ce4577f562bce06040d01248f21aacde1a
Sharor <johannes_christensen@msn.com>
2018-02-14 13:57:32 +0100

    docker-ce

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt-get update
-sudo apt-get -y -q install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get purge -y lxc-docker
-sudo apt-cache policy docker-engine
-sudo apt-get -y -q upgrade
-sudo apt-get -y -q install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get update
-sudo apt-get -y install docker-engine
-sudo service docker start
-sudo usermod -aG docker $USER # remove dependency on sudo access to docker
-
-
+sudo apt-get install docker-ce
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
