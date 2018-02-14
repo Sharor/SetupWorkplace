7e9f4d50ef6f390a4effe1b807c77aeac301060d
Sharor <johannes_christensen@msn.com>
2018-02-14 13:53:58 +0100

    updated dotnet

--- dotnet/install-dotnet-core.sh
+++ dotnet/install-dotnet-core.sh
@@ -1,5 +1,7 @@
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install -y dotnet-dev-1.0.0-preview2.1-003177
-
+sudo apt-get install dotnet-sdk-2.1.4
+dotnet help
